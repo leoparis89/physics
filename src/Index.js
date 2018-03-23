1ce6009ffec997db890139d58f7fc890593157b8
Lev Kowalski <levkowal@publicisgroupe.fr>
2018-03-23 16:43:47 +0100

    example

--- src/Index.js
+++ src/Index.js
@@ -1,16 +1,26 @@
 import './styles/styles.scss';
-import {Engine} from 'matter-js';
+import {Engine, Bodies, World} from 'matter-js';
 // const p5 = require('p5');
 // const myP5 = new p5();
 import 'p5';
 
 // myP5.setup = () => {//   myP5.createCanvas(640, 480)
 // }
+let box, engine, world;
+
+
 window.setup = () => {
   createCanvas(640, 480);
-  const engine = Engine.create();
+  engine = Engine.create();
+  world = engine.world;
+  box = Bodies.rectangle(200, 100, 80, 80);
+  Engine.run(engine);
+  console.log(box);
+  World.add(world, box);
 };
 
 window.draw = () => {
   background(52);
+
+  rect(box.position.x, box.position.y, 80, 80);
 };
