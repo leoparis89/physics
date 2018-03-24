6505d1bf9ae19970c9c1abc6b3656cff1456c00f
levkowalski <levkowalski@gmail.com>
2018-03-24 21:09:25 +0100

    have fun

--- src/Index.js
+++ src/Index.js
@@ -2,7 +2,7 @@ import './styles/styles.scss';
 import {Engine, Bodies, World} from 'matter-js';
 import 'p5';
 
-let box, engine, world;
+let box, engine, world, ground, boxes=[];
 
 
 window.setup = () => {
@@ -11,13 +11,23 @@ window.setup = () => {
   world = engine.world;
   Engine.run(engine);
   // console.log(box);
-  box = new Box(50, 50, 80, 80);
+  // box = new Box(50, 50, 80, 80);
+  ground = Bodies.rectangle(320,480, 640, 20, {isStatic:true});
+  World.add(world, ground);
 };
 
 window.draw = () => {
   background(52);
-  box.show();
+  // box.show();
+  for (let box of boxes) {
+    box.show();
+  }
 };
+
+window.addEventListener('mousedown', ({x, y}) => {
+  // debugger
+  boxes.push(new Box(x, y, random(10, 40), random(10, 40)));
+});
 
 class Box {
   constructor(x, y, w, h) {
@@ -26,6 +36,8 @@ class Box {
     this.w = w;
     this.h = h;
     this.body = Bodies.rectangle(x, y, w, h);
+    this.body.friction = 0;
+    this.body.restitution = 1;
     World.add(world, this.body);
   }
 
@@ -33,6 +45,11 @@ class Box {
     const {position: {x, y}, angle} = this.body;
     push();
     translate(x, y);
+    rotate(angle);
+    rectMode(CENTER);
+    strokeWeight(4);
+    stroke(255);
+    fill(127)
     rect(0, 0, this.w, this.h);
     pop();
   }
